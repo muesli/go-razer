e9289dcd2b6bc86c06ae65f1f4908cfa203f0c19
Christian Muehlhaeuser <muesli@gmail.com>
2018-05-17 06:28:27 +0200

    Lantern supports primary & secondary effect colors

--- cmd/lantern/main.go
+++ cmd/lantern/main.go
@@ -14,15 +14,25 @@ import (
 )
 
 var (
-	col        = flag.String("color", "", "Sets the entire keyboard to this color")
+	primary    = flag.String("color", "#ff0000", "Sets the primary keyboard color")
+	secondary  = flag.String("secondary", "#00ff00", "secondary color (for 'dual' effect modes)")
 	brightness = flag.Float64("brightness", -1, "brightness (between 0 and 100)")
+	effect     = flag.String("effect", "", "effect mode (reactive, wave, spectrum, breath[dual,random], starlight[dual,random], ripple[random])")
+	theme      = flag.String("theme", "", "theme mode (happy, rainbow)")
 	top        = flag.Bool("top", false, "top mode")
-	theme      = flag.String("theme", "", "theme mode")
-	effect     = flag.String("effect", "", "effect mode (wave, spectrum, breath, starlight, ripple)")
 )
 
 func main() {
 	flag.Parse()
+
+	primaryColor, err := colorful.Hex(*primary)
+	if err != nil {
+		panic(err)
+	}
+	secondaryColor, err := colorful.Hex(*secondary)
+	if err != nil {
+		panic(err)
+	}
 
 	devs, err := razer.Devices()
 	if err != nil {
@@ -41,7 +51,16 @@ func main() {
 		d.SetBrightness(*brightness)
 	}
 
-	if *theme == "happy" {
+	if *theme == "rainbow" {
+		pal := colorful.FastHappyPalette(rows)
+		for y := 0; y < rows; y++ {
+			for x := 0; x < cols; x++ {
+				k.Key(y, x).Color = pal[y]
+				d.SetKeys(k)
+				time.Sleep(5 * time.Millisecond)
+			}
+		}
+	} else if *theme == "happy" {
 		k.SetColor(color.RGBA{255, 0, 0, 0})
 		pal := colorful.FastHappyPalette(9)
 		for _, key := range k.FnKeys {
@@ -73,7 +92,7 @@ func main() {
 		}
 		d.SetKeys(k)
 	} else if *effect != "" {
-		d.SetEffect(razer.StringToEffect(*effect))
+		d.SetEffect(razer.NewEffect(razer.StringToEffectType(*effect), primaryColor, secondaryColor))
 	} else if *top {
 		base := 2.5
 		for {
@@ -103,27 +122,7 @@ func main() {
 				base = 3
 			}
 		}
-	} else if *col != "" {
-		c, err := colorful.Hex(*col)
-		if err != nil {
-			panic(err)
-		}
-
-		var a []interface{}
-		r, g, b, _ := c.RGBA()
-		a = append(a, uint8(r>>8))
-		a = append(a, uint8(g>>8))
-		a = append(a, uint8(b>>8))
-
-		d.SetEffect(razer.EffectStatic, a...)
-	} else {
-		pal := colorful.FastHappyPalette(rows)
-		for y := 0; y < rows; y++ {
-			for x := 0; x < cols; x++ {
-				k.Key(y, x).Color = pal[y]
-				d.SetKeys(k)
-				time.Sleep(5 * time.Millisecond)
-			}
-		}
+	} else if *primary != "" {
+		d.SetEffect(razer.NewEffect(razer.EffectStatic, primaryColor, secondaryColor))
 	}
 }
